cdb087ab50ab61279f948379f5a037e4a11e0699
Landfor <tu271766@gmail.com>
2023-05-19 23:11:08 +0900

    11시 일시적으로 풀이한거

--- Homework0519/Homework0519/Homework0519.cpp
+++ Homework0519/Homework0519/Homework0519.cpp
@@ -1,11 +1,138 @@
-﻿// Homework0519.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+﻿// HomeWork0519.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
 //
 
 #include <iostream>
 
+//int ChCount = 0;
+//while (_string[ChCount])
+//{
+//    ++ChCount;
+//}
+
+int ChCount(const char* const _string, char Ch)
+{
+    if (nullptr == _string)
+    {
+        return -1;
+    }
+
+    int Count = 0;
+
+    int ChCount = 0;
+    while (_string[ChCount])
+    {
+        char Ch = _string[ChCount];
+
+        for (int i = 0; Ch == 'a'; i++)
+        {
+            Count++;
+            break;
+        }
+
+        ++ChCount;
+    }
+
+
+    // for문을 돌리는건 당연한거야.
+
+    // Ch와 같은 문자가 몇개들어있는지 세어서 리턴해주는 함수
+    std::cout << Count << "\n";
+
+    return Count;
+}
+
+void TrimDelete(char* _string)
+{
+    int ChCount = 0;
+    char Ren[70] = {};
+    while (_string[ChCount])
+    {
+        char Ch = _string[ChCount];
+        if (Ch == ' ')
+        {
+            Ch = NULL;
+        }
+        Ren[ChCount] = Ch;
+        ++ChCount;
+    }
+
+    *_string = Ren[ChCount];
+
+    return;
+}
+
+
+int DigitsCount(int _Number)
+{
+    return 0;
+}
+
+void StrCopy(const char* const _Left, char* _Right)
+{
+    int alp = 0;
+    char word = _Right[alp];
+    while ((_Right[alp] = *_Left) != '\0')
+
+        return;
+}
+
+// 가장 어려운 숙제
+void NumberToString(int _Number, char* _Right)
+{
+    return;
+}
+
+
 int main()
 {
-    //ㄱㄴㄷㄹㅁㅂㅅvdsfasffsda
+    // 4가 리턴되어야 합니다.
+    // 문자열의 마지막에 들어가는 0은 글자로 치지 않습니다.
+    {
+        int Result = ChCount("ab aaa ccc ddd eee", 'w');
+
+        int Result2 = ChCount(nullptr, 'w');
+    }
+
+    {
+        char Arr[256] = "aa  b  c dd ee";
+
+        TrimDelete(Arr);
+
+        // Arr "aabcddee"
+        int a = 0;
+    }
+
+    {
+        // 8이 리턴되게 만들어라.
+        int Result = DigitsCount(100);//?
+
+        int a = 0;
+    }
+
+    {
+        char ArrCopyText[256] = {};
+
+        StrCopy("aaaa bbb ccc", ArrCopyText);
+
+        int a = 0;
+    }
+
+    {
+        char Result[256] = {};
+
+        // Result = "312312";
+
+        NumberToString(312312, Result);
+
+        char Ch = '0';
+
+        int Value = 7;
+
+        char ChConvert = Value + 48;
+
+        int a = 0;
+    }
+
     std::cout << "Hello World!\n";
 }
 
