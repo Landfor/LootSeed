e19aa2b8df1e57f70875376f7397471aae15bd8e
Landfor <tu271766@gmail.com>
2023-05-25 23:20:46 +0900

    0525 숙제 해결

---
+++ Homework0519/Homework23/Monster.cpp
@@ -0,0 +1,16 @@
+#include "Monster.h"#include "Player.h"
+
+Monster::Monster()
+{
+
+}
+
+void Monster::TestMonster()
+{
+
+}
+
+void Monster::Fight(Player* _Monster)
+{
+	_Monster->TestPlayer();
+}
