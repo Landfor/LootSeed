7e817cdf9e83c8c2fcc1ea9c4a1e84b2558474dc
Landfor <tu271766@gmail.com>
2023-06-14 12:16:01 +0900

    임시 언리얼

---
+++ Starter06122/Source/Starter06122/Starter06122GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "Starter06122GameModeBase.h"
+
---
+++ Starter06122/Source/Starter06122/Starter06122GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Starter06122GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARTER06122_API AStarter06122GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
