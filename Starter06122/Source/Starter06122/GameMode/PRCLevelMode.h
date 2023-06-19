e2d5c96a2d5194b72c90a4d51afc07de66e1c0b3
Landfor <tu271766@gmail.com>
2023-06-19 12:53:57 +0900

    언리얼 0619

---
+++ Starter06122/Source/Starter06122/GameMode/PRCLevelMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "PRCLevelMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARTER06122_API APRCLevelMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
